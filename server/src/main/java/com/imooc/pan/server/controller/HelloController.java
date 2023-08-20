bcf011018fe0b4687252f5f7958bddde84c25d49
JiangTaoHou <925147954@qq.com>
2023-08-20 23:18:11 +0800

    1.在swagger中不点击全选就好了，那么就不是传body了

--- server/src/main/java/com/imooc/pan/server/controller/HelloController.java
+++ server/src/main/java/com/imooc/pan/server/controller/HelloController.java
@@ -11,7 +11,7 @@ import javax.validation.constraints.NotBlank;
 @RestController
 public class HelloController {
     @GetMapping("hello")
-    public R<String> hello(@RequestParam(name = "name")@NotBlank(message = "name不能为空") String name){
+    public R<String> hello(@NotBlank(message = "name不能为空") String name){
         System.out.println(Thread.currentThread().getContextClassLoader());
         return R.success("hello" +name +"1");
     }
