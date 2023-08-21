5d10ed7b3ce6aad987e8072ac8d73cf06d009cf1
JiangTaoHou <925147954@qq.com>
2023-08-21 21:55:41 +0800

    1.增加代码生成器

---
+++ framework/orm/mybatis-plus/src/test/java/com/imooc/pan/orm/mp/generate/MpGenaratorTest.java
@@ -0,0 +1,19 @@
+package com.imooc.pan.orm.mp.generate;
+
+public class MpGenaratorTest {
+
+    /**
+     * 代码生成器的测试方法
+     * root
+     * root
+     * com.imooc.pan.modules.user
+     * D:\devCode\r-pan\framework\orm\mybatis-plus\src\test\java\com\imooc\pan\orm\mp\generate\output
+     * r_pan_user
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        CodeGenerator.generate();
+    }
+
+}
